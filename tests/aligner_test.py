f5c7cf7dc7867d7c6461e028fcd371865a4c7240
anova02 <anova02.regentshouse@gmail.com>
2024-07-15 16:48:44 -0700

    Optimize code, disable logger

--- tests/aligner_test.py
+++ tests/aligner_test.py
@@ -1,15 +1,18 @@
 # not even a pytest
 
 from src.aligner import Aligner
-from src.alignments import Alignments
+from src.alignments import Alignments, bindings_logger
 
 uinput = ""
 
 print('oh hello there. enter a phrase to align, or ".exit" to quit.')
-while uinput != ".exit":
+while True:
     uinput = input('>>> ')
     if uinput == ".exit":
         break
     else:
+        bindings_logger.disabled = True
+        m2m_aligner_output = Aligner._m2m_align(Aligner._word_phoneme_pairs_g2p_en(uinput))
         alignments = Alignments._from_aligner_output(Aligner.align(uinput))
+        print(m2m_aligner_output)
         print(alignments)
